5503d356fb9536e019054d20c3cb90294cc01767
Akshanshkaushal <akshanshkaushal9@gmail.com>
2023-12-28 02:22:09 +0530

    cors adde

--- src/context/AccountProvider.jsx
+++ src/context/AccountProvider.jsx
@@ -16,7 +16,7 @@ const AccountProvider = ({children}) => {
     const socket = useRef();
 
     useEffect(() => {
-        socket.current = io('https://whatsappclone-socket-backendorigin.onrender.com');
+        socket.current = io('https://whatsappclone-socket-backend4.onrender.com');
     }, [])
 
     return (
